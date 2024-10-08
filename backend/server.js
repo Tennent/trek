883629b416a601efa98d27975ae08ea11bb0bdeb
Tennent <zalan.marton@gmail.com>
2024-10-08 10:37:28 +0200

    Construct directory path

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,10 @@
 import express from "express";
+import { fileURLToPath } from 'url';
+import path from "path";
+
+// Construct directory path
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
