39d945a2c7db07137bd96eff2c0f288296ffd171
Tennent <zalan.marton@gmail.com>
2024-10-08 13:07:24 +0200

    Define function to log in user

--- backend/database/models/UserModel.js
+++ backend/database/models/UserModel.js
@@ -37,4 +37,26 @@ userSchema.statics.signup = async function (user_name, password, email) {
     return user
 }
 
+userSchema.statics.login = async function (user_name, password) {
+    
+    if (!user_name || !password) {
+        throw Error("All fields must be filled")
+    }
+
+    const user = await this.findOne({ user_name })
+
+    if (!user) {
+        throw Error("Invalid user name")
+    }
+
+    const matchPassword = bcrypt.compareSync(password, user.hashed_password)
+
+    if (!matchPassword) {
+        throw Error("Invalid login details")
+    }
+
+    const token = { loggedIn: true, admin: user.is_admin, _id: user._id, user_name: user.user_name }
+    return token;
+}
+
 export default mongoose.model('user', userSchema, 'users');
