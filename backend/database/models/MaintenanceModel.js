e5cf3642dd3a227e2c1224b49428eacea0b311e5
Tennent <zalan.marton@gmail.com>
2024-10-10 09:38:13 +0200

    Define function for creating maitenance entry and extend schema with car

--- backend/database/models/MaintenanceModel.js
+++ backend/database/models/MaintenanceModel.js
@@ -9,7 +9,21 @@ const maintenanceSchema = new Schema({
         type: Schema.Types.ObjectId,
         ref: "ItemModel",
         required: true
-    }]
+    }],
+    car: {
+        type: Schema.Types.ObjectId,
+        ref: "CarModel",
+        required: true
+    }
 });
+
+maintenanceSchema.statics.createMaintenanceEntry = async function (date, titel, description, cost, items, car) {
+    if (!date || !titel || !cost || !car) {
+        throw Error("All mandatory fields must be filled");
+    };
+
+    const maintenance = this.create({ date, titel, description, cost, items, car });
+    return maintenance;
+}
 
 export default mongoose.model('maintenance', maintenanceSchema, 'maintenances');
