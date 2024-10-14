da0e3eefc2fa183f4fd0043d46a0e5072f94763a
Tennent <zalan.marton@gmail.com>
2024-10-14 13:38:54 +0200

    Import page style and remove logo

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import './Navbar.css'
 
 export default function Navbar() {
     return (
         <nav className='navbar'>
             <div className='navbar-logo'>
-                <Link to={'/'}>
-                    <img src="/trek-logo.png" alt="trek-logo" />
-                </Link>
+                <Link to={'/'}></Link>
             </div>
             <div className='navbar-links'>
                 <Link to='/'>Home</Link>
