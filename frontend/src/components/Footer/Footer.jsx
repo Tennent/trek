a0b4e9f7c6192bd12b9ddd08e9c94cb26edc6670
Tennent <zalan.marton@gmail.com>
2024-10-15 14:13:46 +0200

    Create structure for footer elements

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,18 @@
+export default function Footer() {
+    return (
+        <nav className='footer-container'>
+            <h1 className="call-to-action">
+                Register your car now!
+            </h1>
+            <div className='social-links'>
+                <img src="/icons/fb-icon.png" alt="fb-icon" />
+                <img src="/icons/insta-icon.png" alt="insta-icon" />
+                <img src="/icons/linkedin-icon.png" alt="linkedin-icon" />
+            </div>
+            <div className="legal-links">
+                <p>Privacy policy</p>
+                <p>Terms & conditions</p>
+            </div>
+        </nav>
+    )
+}
