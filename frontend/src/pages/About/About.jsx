f06008c1cb102eddd5c1aefed2e5723faea28455
Tennent <zalan.marton@gmail.com>
2024-10-16 10:55:23 +0200

    Create structure for about page elements

---
+++ frontend/src/pages/About/About.jsx
@@ -0,0 +1,10 @@
+export default function About() {
+    return (
+        <div className='about-container'>
+            <div className="about-content">
+                <h1 className="about-title">About trek</h1>
+                <p className="about-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum dicta id illum veniam dolores unde, ullam debitis ea blanditiis beatae perferendis expedita laboriosam sapiente, commodi earum pariatur vel ab. Omnis.</p>
+            </div>
+        </div>
+    )
+}
