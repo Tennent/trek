1f2feb0aac415ef5e2fb2ec2a05b394e62cf5840
Tennent <zalan.marton@gmail.com>
2024-10-16 11:07:55 +0200

    Refactor structure and add highlight element for title

--- frontend/src/pages/About/About.jsx
+++ frontend/src/pages/About/About.jsx
@@ -1,9 +1,15 @@
+import './About.css'
+
 export default function About() {
     return (
         <div className='about-container'>
             <div className="about-content">
-                <h1 className="about-title">About trek</h1>
-                <p className="about-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum dicta id illum veniam dolores unde, ullam debitis ea blanditiis beatae perferendis expedita laboriosam sapiente, commodi earum pariatur vel ab. Omnis.</p>
+                <h1 className="about-title">
+                    About <span className="highlight">trek</span>
+                </h1>
+                <p className="about-text">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum dicta id illum veniam dolores unde, ullam debitis ea blanditiis beatae perferendis expedita laboriosam sapiente, commodi earum pariatur vel ab. Omnis.
+                </p>
             </div>
         </div>
     )
