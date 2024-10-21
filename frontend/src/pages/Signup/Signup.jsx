289468dd4f15b8242d23a452561743d1ee424e86
Tennent <zalan.marton@gmail.com>
2024-10-21 10:10:31 +0200

    Define placeholder function to handle form submit

--- frontend/src/pages/Signup/Signup.jsx
+++ frontend/src/pages/Signup/Signup.jsx
@@ -1,10 +1,16 @@
 import Modal from "react-modal";
 
 export default function Signup({ isOpen, onClose }) {
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("Form submitted");
+    }
+
     return (
         <Modal isOpen={isOpen} onRequestClose={onClose} className="custom-modal" overlayClassName="custom-overlay">
             <div className="signup-container">
-                <form className='signup-form'>
+                <form className='signup-form' onSubmit={handleSubmit}>
                     <label htmlFor="userName">Email:</label>
                     <input id="userEmail" name="userEmail" type="text" />
                     <label htmlFor="userPassword">Password:</label>
