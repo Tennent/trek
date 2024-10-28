150e8e0cdf24224b2f82444e422b37b289e5a07d
Tennent <zalan.marton@gmail.com>
2024-10-28 13:47:59 +0100

    Add icon and image references

--- frontend/src/pages/Manage/Manage.jsx
+++ frontend/src/pages/Manage/Manage.jsx
@@ -1,3 +1,5 @@
+import "./Manage.css";
+
 export default function Manage() {
     return (
         <div className='manage-container'>
@@ -6,7 +8,7 @@ export default function Manage() {
             </div>
 
             <div className="card-container">
-                <img className="card-image" src="" alt="" />
+                <img className="card-image" src="./dummy-user-car.png" alt="user-car-image" />
                 <div className="card-list-container">
                     <ul className="card-list">
                         <li>
@@ -26,18 +28,18 @@ export default function Manage() {
 
                 <div className="card-menu-container">
                     <div className="add-option">
-                        <img src="" alt="" />
+                        <img src="./icons/manage-add-icon.png" alt="add-icon" />
                     </div>
                     <div className="edit-option">
-                        <img src="" alt="" />
+                        <img src="./icons/manage-edit-icon.png" alt="edit-icon" />
                     </div>
                     <div className="delete-option">
-                        <img src="" alt="" />
+                        <img src="./icons/manage-delete-icon.png" alt="delete-icon" />
                     </div>
                 </div>
             </div>
 
-            <img className="add-item" src="" alt="" />
+            <img className="add-item" src="./icons/manage-add-item-icon.png" alt="add-item-icon" />
         </div>
     )
 }
