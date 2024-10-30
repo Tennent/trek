f1a6d6b803d09f8be1f58b16ee698d19148aeb60
Tennent <zalan.marton@gmail.com>
2024-10-30 11:29:56 +0100

    Create container for maintenance and operation cards

--- frontend/src/pages/Save/Save.jsx
+++ frontend/src/pages/Save/Save.jsx
@@ -12,81 +12,83 @@ export default function Save() {
                 </h2>
             </div>
 
-            <div className="save-maintenance-card">
-                <h2 className="save-maintenance-title">
-                    Maintenance
-                </h2>
-                <div className="save-maintenance-period-container">
-                    <p className="save-maintenance-period">
-                        Weekly
-                    </p>
-                    <p className="save-maintenance-period">
-                        Monthly
-                    </p>
-                    <p className="save-maintenance-period">
-                        Yearly
-                    </p>
+            <div className="save-card-container">
+                <div className="save-maintenance-card">
+                    <h2 className="save-maintenance-title">
+                        Maintenance
+                    </h2>
+                    <div className="save-maintenance-period-container">
+                        <p className="save-maintenance-period">
+                            Weekly
+                        </p>
+                        <p className="save-maintenance-period">
+                            Monthly
+                        </p>
+                        <p className="save-maintenance-period">
+                            Yearly
+                        </p>
+                    </div>
+                    <div className="save-maitenance-details-container">
+                        <ul className="save-maintenance-details-list">
+                            <li className="save-maintenance-detail-title">
+                                Service
+                            </li>
+                            <li className="save-maintenance-detail-item">
+                                Scheduled
+                            </li>
+                            <li className="save-maintenance-detail-item">
+                                Unscheduled
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="save-maintenance-chart"></div>
                 </div>
-                <div className="save-maitenance-details-container">
-                    <ul className="save-maintenance-details-list">
-                        <li className="save-maintenance-detail-title">
-                            Service
-                        </li>
-                        <li className="save-maintenance-detail-item">
-                            Scheduled
-                        </li>
-                        <li className="save-maintenance-detail-item">
-                            Unscheduled
-                        </li>
-                    </ul>
-                </div>
-                <div className="save-maintenance-chart"></div>
-            </div>
 
-            <div className="save-operation-card">
-                <h2 className="save-operation-title">
-                    Operation
-                </h2>
-                <div className="save-operation-period-container">
-                    <p className="save-operation-period">
-                        Weekly
-                    </p>
-                    <p className="save-operation-period">
-                        Monthly
-                    </p>
-                    <p className="save-operation-period">
-                        Yearly
-                    </p>
+                <div className="save-operation-card">
+                    <h2 className="save-operation-title">
+                        Operation
+                    </h2>
+                    <div className="save-operation-period-container">
+                        <p className="save-operation-period">
+                            Weekly
+                        </p>
+                        <p className="save-operation-period">
+                            Monthly
+                        </p>
+                        <p className="save-operation-period">
+                            Yearly
+                        </p>
+                    </div>
+                    <div className="save-operation-details-container">
+                        <ul className="save-operation-details-list">
+                            <li className="save-operation-detail-title">
+                                Fuel
+                            </li>
+                            <li className="save-operation-detail-title">
+                                Parking
+                            </li>
+                            <li className="save-operation-detail-title">
+                                Tax
+                            </li>
+                            <li className="save-operation-detail-title">
+                                Insurance
+                            </li>
+                            <li className="save-operation-detail-title">
+                                Fine
+                            </li>
+                            <li className="save-operation-detail-item">
+                                Parking
+                            </li>
+                            <li className="save-operation-detail-item">
+                                Speeding
+                            </li>
+                            <li className="save-operation-detail-item">
+                                Other
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="save-operation-chart"></div>
                 </div>
-                <div className="save-operation-details-container">
-                    <ul className="save-operation-details-list">
-                        <li className="save-operation-detail-title">
-                            Fuel
-                        </li>
-                        <li className="save-operation-detail-title">
-                            Parking
-                        </li>
-                        <li className="save-operation-detail-title">
-                            Tax
-                        </li>
-                        <li className="save-operation-detail-title">
-                            Insurance
-                        </li>
-                        <li className="save-operation-detail-title">
-                            Fine
-                        </li>
-                        <li className="save-operation-detail-item">
-                            Parking
-                        </li>
-                        <li className="save-operation-detail-item">
-                            Speeding
-                        </li>
-                        <li className="save-operation-detail-item">
-                            Other
-                        </li>
-                    </ul>
-                </div>
-                <div className="save-operation-chart"></div>
             </div>
         </div>
     )
