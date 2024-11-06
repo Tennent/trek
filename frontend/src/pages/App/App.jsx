dec2cac4179b856ee98659ced82d085506051751
Tennent <zalan.marton@gmail.com>
2024-11-06 13:58:33 +0100

    Import login component and pass user state for layout

--- frontend/src/pages/App/App.jsx
+++ frontend/src/pages/App/App.jsx
@@ -6,6 +6,7 @@ import Home from "../Home/Home"
 import About from "../About/About";
 import Contact from "../Contact/Contact";
 import Signup from "../Signup/Signup";
+import Login from "../Login/Login";
 import Manage from "../Manage/Manage";
 import Track from "../Track/Track";
 import Save from "../Save/Save";
@@ -15,11 +16,12 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout user={user} />}>
+        <Route path="/" element={<Layout user={user} setUser={setUser} />}>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="contact" element={<Contact />} />
-          <Route path="signup" element={<Signup user={user} setUser={setUser} />} />
+          <Route path="signup" element={<Signup />} />
+          <Route path="login" element={<Login />} />
           <Route path="manage" element={<Manage />} />
           <Route path="track" element={<Track />} />
           <Route path="save" element={<Save />} />
