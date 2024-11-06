5825a65011169ac2c33a000ce35d68b0fb110173
Tennent <zalan.marton@gmail.com>
2024-11-06 13:01:16 +0100

    Import state and navigate components

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,7 +1,10 @@
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 import Modal from "react-modal";
 import "./Login.css";
 
 export default function Login({ isOpen, onClose, onSwitch }) {
+    const navigate = useNavigate();
     const [userEmail, setUserEmail] = useState("");
     const [userPassword, setUserPassword] = useState("");
 
