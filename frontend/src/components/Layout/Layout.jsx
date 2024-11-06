ece4a9175adb51687c8e0ddbb0c6c7abbe29904c
Tennent <zalan.marton@gmail.com>
2024-11-06 13:58:50 +0100

    Pass user state for navbar

--- frontend/src/components/Layout/Layout.jsx
+++ frontend/src/components/Layout/Layout.jsx
@@ -3,11 +3,11 @@ import { Outlet } from "react-router-dom";
 import Navbar from "../Navbar/Navbar";
 import Footer from "../Footer/Footer";
 
-export default function Layout({ user }) {
+export default function Layout({ user, setUser }) {
   return (
     <>
       <main id='main'>
-        <Navbar user={user} />
+        <Navbar user={user} setUser={setUser} />
         <Outlet />
         <Footer />
       </main>
