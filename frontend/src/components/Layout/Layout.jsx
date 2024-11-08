485470feb411ba346cbb98f47c07a39412fb90c0
Tennent <zalan.marton@gmail.com>
2024-11-08 09:04:36 +0100

    Pass car id state for navbar

--- frontend/src/components/Layout/Layout.jsx
+++ frontend/src/components/Layout/Layout.jsx
@@ -3,11 +3,11 @@ import { Outlet } from "react-router-dom";
 import Navbar from "../Navbar/Navbar";
 import Footer from "../Footer/Footer";
 
-export default function Layout({ user, setUser }) {
+export default function Layout({ user, setUser, setUserCarIds }) {
   return (
     <>
       <main id='main'>
-        <Navbar user={user} setUser={setUser} />
+        <Navbar user={user} setUser={setUser} setUserCarIds={setUserCarIds} />
         <Outlet />
         <Footer user={user} />
       </main>
