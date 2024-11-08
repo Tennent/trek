08f00b34da830bd1daadb73796d936f93a4755fc
Tennent <zalan.marton@gmail.com>
2024-11-08 14:15:03 +0100

    Migrate car list to external component

--- frontend/src/pages/Manage/Manage.jsx
+++ frontend/src/pages/Manage/Manage.jsx
@@ -1,42 +1,11 @@
 import { useState, useEffect } from "react";
 import fetchUserCar from "../../services/fetchUserCar";
+import CarList from "../CarList/CarList";
 import "./Manage.css";
-
-function UserCarList({ cars }) {
-    return (
-        <div className="manage-card-container-list">
-            {cars.map((car, index) => (
-                <div key={car._id || index} className="card-container">
-                    <img className="card-image" src="./dummy-user-car.png" alt="user-car-image" />
-                    <div className="card-list-container">
-                        <ul className="card-list">
-                            <li>NICKNAME:</li>
-                            <li>YEAR: {car.year}</li>
-                            <li>MAKE: {car.make}</li>
-                            <li>MODEL: {car.model}</li>
-                        </ul>
-                    </div>
-
-                    <div className="card-menu-container">
-                        <div className="add-option">
-                            <img src="./icons/manage-add-icon.png" alt="add-icon" />
-                        </div>
-                        <div className="edit-option">
-                            <img src="./icons/manage-edit-icon.png" alt="edit-icon" />
-                        </div>
-                        <div className="delete-option">
-                            <img src="./icons/manage-delete-icon.png" alt="delete-icon" />
-                        </div>
-                    </div>
-                </div>
-            ))}
-        </div>
-    );
-};
 
 export default function Manage({ userCarIds, userCars, setUserCars }) {
     const [isHovered, setIsHovered] = useState(false);
-
+    
     useEffect(() => {
         async function collectUserCars() {
             try {
@@ -68,7 +37,7 @@ export default function Manage({ userCarIds, userCars, setUserCars }) {
 
             {userCars.length > 0
                 ? <>
-                    <UserCarList cars={userCars} />
+                    <CarList userCars={userCars} setUserCars={setUserCars} manageModalState={manageModalState} openModal={openModal} closeModal={closeModal} />
                     <div
                         className="add-item-container"
                         onMouseEnter={() => setIsHovered(true)}
