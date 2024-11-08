8cbcf8ca850d31e2ece695b940680673c495e84c
Tennent <zalan.marton@gmail.com>
2024-11-08 14:22:52 +0100

    Define function to handle form input changes

--- frontend/src/pages/CarEdit/CarEdit.jsx
+++ frontend/src/pages/CarEdit/CarEdit.jsx
@@ -10,20 +10,28 @@ export default function CarEdit({ carId, userCars, setUserCars, manageModalState
         body_type: ''
     });
 
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData(prev => ({
+            ...prev,
+            [name]: value
+        }));
+    };
+
     return (
         <Modal isOpen={manageModalState.isOpen} onRequestClose={closeModal} className="custom-modal" overlayClassName="custom-overlay">
             <div className="car-edit-container">
                 <form className='car-edit-form'>
                     <label htmlFor="car-year">Year:</label>
-                    <input id="car-year" name="car-year" type="number" value={formData.year} />
+                    <input id="car-year" name="car-year" type="number" value={formData.year} onChange={handleChange} />
                     <label htmlFor="car-make">Make:</label>
-                    <input id="car-make" name="car-make" type="text" value={formData.make} />
+                    <input id="car-make" name="car-make" type="text" value={formData.make} onChange={handleChange} />
                     <label htmlFor="car-model">Model:</label>
-                    <input id="car-model" name="car-model" type="text" value={formData.model} />
+                    <input id="car-model" name="car-model" type="text" value={formData.model} onChange={handleChange} />
                     <label htmlFor="car-fuel">Fuel Type:</label>
-                    <input id="car-fuel" name="car-fuel" type="text" value={formData.fuel_type} />
+                    <input id="car-fuel" name="car-fuel" type="text" value={formData.fuel_type} onChange={handleChange} />
                     <label htmlFor="car-body">Body Type:</label>
-                    <input id="car-body" name="car-body" type="text" value={formData.body_type} />
+                    <input id="car-body" name="car-body" type="text" value={formData.body_type} onChange={handleChange} />
 
                     <div className='save-btn-container'>
                         <button type="submit" className="save-button">Save</button>
