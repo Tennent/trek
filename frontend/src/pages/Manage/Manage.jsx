c9b1993bc68643beacfed739e7092d44e87aa6f1
Tennent <zalan.marton@gmail.com>
2024-11-08 10:07:07 +0100

    Create component for car info card list

--- frontend/src/pages/Manage/Manage.jsx
+++ frontend/src/pages/Manage/Manage.jsx
@@ -1,6 +1,38 @@
 import { useState } from "react";
 import "./Manage.css";
 
+function UserCarList({ cars }) {
+    return (
+        <div className="manage-card-container-list">
+            {cars.map((car, index) => (
+                <div key={car._id || index} className="card-container">
+                    <img className="card-image" src="./dummy-user-car.png" alt="user-car-image" />
+                    <div className="card-list-container">
+                        <ul className="card-list">
+                            <li>NICKNAME:</li>
+                            <li>YEAR: {car.year}</li>
+                            <li>MAKE: {car.make}</li>
+                            <li>MODEL: {car.model}</li>
+                        </ul>
+                    </div>
+
+                    <div className="card-menu-container">
+                        <div className="add-option">
+                            <img src="./icons/manage-add-icon.png" alt="add-icon" />
+                        </div>
+                        <div className="edit-option">
+                            <img src="./icons/manage-edit-icon.png" alt="edit-icon" />
+                        </div>
+                        <div className="delete-option">
+                            <img src="./icons/manage-delete-icon.png" alt="delete-icon" />
+                        </div>
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
+};
+
 export default function Manage() {
     const [isHovered, setIsHovered] = useState(false);
 
@@ -8,38 +40,6 @@ export default function Manage() {
         <div className='manage-container'>
             <div className="manage-title">
                 <h1>MANAGE</h1>
-            </div>
-
-            <div className="card-container">
-                <img className="card-image" src="./dummy-user-car.png" alt="user-car-image" />
-                <div className="card-list-container">
-                    <ul className="card-list">
-                        <li>
-                            NICKNAME:
-                        </li>
-                        <li>
-                            YEAR:
-                        </li>
-                        <li>
-                            MAKE:
-                        </li>
-                        <li>
-                            MODEL:
-                        </li>
-                    </ul>
-                </div>
-
-                <div className="card-menu-container">
-                    <div className="add-option">
-                        <img src="./icons/manage-add-icon.png" alt="add-icon" />
-                    </div>
-                    <div className="edit-option">
-                        <img src="./icons/manage-edit-icon.png" alt="edit-icon" />
-                    </div>
-                    <div className="delete-option">
-                        <img src="./icons/manage-delete-icon.png" alt="delete-icon" />
-                    </div>
-                </div>
             </div>
 
             <div
