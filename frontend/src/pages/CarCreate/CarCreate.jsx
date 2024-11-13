419058e8d4fbd27a62b20f22291b29721ba428f2
Tennent <zalan.marton@gmail.com>
2024-11-13 13:47:40 +0100

    Define state for form data

--- frontend/src/pages/CarCreate/CarCreate.jsx
+++ frontend/src/pages/CarCreate/CarCreate.jsx
@@ -1,21 +1,30 @@
+import { useState } from "react";
 import Modal from "react-modal";
 import "./CarCreate.css";
 
 export default function CarCreate({ userId, userCarIds, setUserCarIds, manageModalState, closeModal }) {
+    const [formData, setFormData] = useState({
+        year: '',
+        make: '',
+        model: '',
+        fuel_type: '',
+        body_type: ''
+    });
+
     return (
         <Modal isOpen={manageModalState.isOpen} onRequestClose={closeModal} className="create-custom-modal" overlayClassName="create-custom-overlay">
             <div className="car-create-container">
                 <form className='car-create-form'>
                     <label htmlFor="year">Year:</label>
-                    <input id="year" name="year" type="number" />
+                    <input id="year" name="year" type="number" value={formData.year} />
                     <label htmlFor="make">Make:</label>
-                    <input id="make" name="make" type="text" />
+                    <input id="make" name="make" type="text" value={formData.make} />
                     <label htmlFor="model">Model:</label>
-                    <input id="model" name="model" type="text" />
+                    <input id="model" name="model" type="text" value={formData.model} />
                     <label htmlFor="fuel_type">Fuel Type:</label>
-                    <input id="fuel_type" name="fuel_type" type="text" />
+                    <input id="fuel_type" name="fuel_type" type="text" value={formData.fuel_type} />
                     <label htmlFor="body_type">Body Type:</label>
-                    <input id="body_type" name="body_type" type="text" />
+                    <input id="body_type" name="body_type" type="text" value={formData.body_type} />
 
                     <div className='create-btn-container'>
                         <button type="submit" className="create-button">Create</button>
